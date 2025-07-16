94f931c016767e3cb8df9470f7907e209b28d825
João Vivas <filipe.vivas.fv@gmail.com>
2025-07-16 16:57:56 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+export default function App(){
+    return(
+        <h>
+            <h1>Hello world</h1>
+            <p>This is a test page</p>
+            <p>Haha</p>
+        </h>
+    );
+};
