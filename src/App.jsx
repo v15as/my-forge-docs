d12ea28b4ecc9323672d34eabc21f7100d2a65d2
João Vivas <filipe.vivas.fv@gmail.com>
2025-07-16 18:37:20 +0100

    now is everything configured with gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { useState } from 'react'
 import './App.css'
 
-export default function App(){
-    return(
-        <h>
-            <h1>Hello world</h1>
-            <p>This is a test page</p>
-            <p>Haha</p>
-        </h>
+function App() {
+
+    return (
+        <>
+            <h1>
+                Hello world
+            </h1>
+            <p>
+                Testing 1 2 3...
+            </p>
+        </>
     );
-};
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router';
-import './index.css';
-import App from './App.jsx';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <StrictMode>
     <App />
-  </BrowserRouter>,
+  </StrictMode>,
 )
